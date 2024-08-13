6072505fc78e370abee67b4b20fd0be156e5d7c7
arthur voryshev <what_to_write@mail.ru>
2024-08-13 15:51:01 +1000

    update  navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import NavBar from "./NavBar";
-// import './App.sass'
-// import './App.css'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
-function App() {
+const App = () => {
   return (
     <NavBar />
+
   );
 }
 
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,6 +1,23 @@
 import { Button, Form } from 'react-bootstrap';
+import React, { useState } from 'react';
+
+// import repos from './const';
+
+
+// const searchRepo = () => {
+//   // const response = repos.map((repo) => repo === value);
+//   console.log(value)
+// }
 
 const NavBar = () => {
+
+  const [value, setValue] = useState('');
+  // console.log(value)
+
+  // const searchRepo = (e) => {
+  //   console.log(e.target.value)
+  // }
+
   return (
     <nav className="shadow-sm navbar navbar-expand-lg navbar-light">
       <Form className="d-flex">
@@ -9,8 +26,10 @@ const NavBar = () => {
           placeholder="Введите поисковый запрос"
           className="me-2"
           aria-label="Search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
         />
-        <Button variant="primary" className="btn">ИСКАТЬ</Button>
+        <Button variant="primary" className="btn" onClick={() => console.log(value)}>ИСКАТЬ</Button>
       </Form>
     </nav >
   )
